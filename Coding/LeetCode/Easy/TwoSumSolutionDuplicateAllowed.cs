b5b5f423a8eb41134e3574c33ee7824ec80dd0e8
Surbhi Sneha <surbhisneha.sneha4@gmail.com>
2020-04-18 12:15:41 +0530

    TwoSumSolutionDuplicateAllowed

---
+++ Coding/LeetCode/Easy/TwoSumSolutionDuplicateAllowed.cs
@@ -0,0 +1,44 @@
+﻿//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//Example:
+
+//Given nums = [2, 7, 11, 15], target = 9,
+
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    class TwoSumSolutionDuplicateAllowed
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+            var sumSetIndex = new int[2];
+            for (int i=0;i<nums.Length;i++)
+            {                
+                dict.Add(nums[i], i);                              
+            }
+
+            foreach (var num in nums)
+            {
+                if (dict.ContainsKey(num))
+                {                    
+                    if(dict.ContainsKey(target - num))
+                    {
+                        sumSetIndex[0] = dict[num];
+                        sumSetIndex[1] = dict[target - num];                            
+                    }                    
+                }
+            }
+
+            return sumSetIndex;
+        }
+    }
+}
---
+++ Coding/LeetCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Easy;
+using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var twoSum = new TwoSumSolutionDuplicateAllowed();
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            var res = twoSum.TwoSum(nums, target);
+        }
+    }
+}
