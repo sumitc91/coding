2e0b21bb5a82ef7664138a2d85535bffcb5b91bc
Surbhi Sneha <surbhisneha.sneha4@gmail.com>
2020-04-18 12:48:47 +0530

    Two sum problem ( With Repeating Numbers )

---
+++ Coding/LeetCode/Easy/TwoSumSolution.cs
@@ -0,0 +1,40 @@
+﻿//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//Example:
+
+//Given nums = [2, 7, 11, 15], target = 9,
+
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var res = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {                
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if(nums[i]+nums[j]==target)
+                    {
+                        res[0] = i;
+                        res[1] = j;
+                        return res;
+                    }
+                }
+
+            }
+
+            return res;
+        }
+    }
+}
